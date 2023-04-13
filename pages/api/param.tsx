e4b4d23d611a266b1ee950e11ec474557ea69425
Iván Peña Huguet <65863620+IvanPenyaHuguet@users.noreply.github.com>
2023-04-14 00:23:00 +0200

    Update param.tsx

--- pages/api/param.tsx
+++ pages/api/param.tsx
@@ -13,57 +13,70 @@ export default function handler(req: NextRequest) {
     const hasTitle = searchParams.has('title')
     const title = hasTitle
       ? searchParams.get('title')?.slice(0, 100)
-      : 'My default title'
+      : 'My default title';
+    const number = searchParams.get('number');
 
     return new ImageResponse(
       (
         <div
-          style={{
-            backgroundColor: 'black',
-            backgroundSize: '150px 150px',
-            height: '100%',
-            width: '100%',
+      style={{
+        backgroundColor: 'white',
+        backgroundSize: '150px 150px',
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        flexWrap: 'nowrap',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          alignContent: 'center',
+          justifyContent: 'center',
+          justifyItems: 'center',
+          flexDirection: 'row'
+        }}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          alt="Vercel"
+          height={200}
+          src={`https://www.iberia.com/wcs_statics/images/modules/ibe_cards/ibe-card-plus-normal.png`}
+          style={{ margin: '0 30px' }}
+          width={232}
+        />
+      
+      <div
+        style={{
+          fontSize: 60,
+          fontStyle: 'normal',
+          letterSpacing: '-0.025em',
+          color: 'black',
+          marginTop: 30,
+          padding: '0 120px',
+          lineHeight: 1.4,
+          whiteSpace: 'pre-wrap',
+        }}
+      >
+        { title }
+      </div>
+      </div>
+      <div style={{
             display: 'flex',
-            textAlign: 'center',
             alignItems: 'center',
+            alignContent: 'center',
             justifyContent: 'center',
-            flexDirection: 'column',
-            flexWrap: 'nowrap',
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              alignContent: 'center',
-              justifyContent: 'center',
-              justifyItems: 'center',
-            }}
-          >
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              alt="Vercel"
-              height={200}
-              src="data:image/svg+xml,%3Csvg width='116' height='100' fill='white' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M57.5 0L115 100H0L57.5 0z' /%3E%3C/svg%3E"
-              style={{ margin: '0 30px' }}
-              width={232}
-            />
-          </div>
-          <div
-            style={{
-              fontSize: 60,
-              fontStyle: 'normal',
-              letterSpacing: '-0.025em',
-              color: 'white',
-              marginTop: 30,
-              padding: '0 120px',
-              lineHeight: 1.4,
-              whiteSpace: 'pre-wrap',
-            }}
-          >
-            {title}
-          </div>
-        </div>
+            justifyItems: 'center',
+            flexDirection: 'row'
+      }}>
+        <span>Mis viajes { number }</span>
+      </div>
+   </div>
       ),
       {
         width: 1200,
